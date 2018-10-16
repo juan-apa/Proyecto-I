05237727b6aa722a30e131ccdf17370008ae3b02
Juan Aparicio <apariciojuan30@gmail.com>
2018-10-16 12:01:50 -0300

    modificacion generarWar

--- generarWar.bat
+++ generarWar.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk1.8.0_181\bin\jar.exe" -cvf .\build\dwr_test.war .\dwr_test\web\*
+"C:\Program Files\Java\jdk1.8.0_181\bin\jar.exe" -cvf .\dwr_test\build\dwr_test.war .\dwr_test\web\*
